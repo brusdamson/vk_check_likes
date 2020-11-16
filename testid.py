0b9fd6644091d73b91b246f2f5ff46265ba63306
brusdamson <74505655+brusdamson@users.noreply.github.com>
2020-11-16 18:19:12 +0300

    Update testid.py

--- testid.py
+++ testid.py
@@ -8,31 +8,30 @@ session = vk.Session(access_token=access_token)
 vkapi = vk.API(session, v = "5.126")
 def getUserId(link):
     id = link
-    if 'vk.com/' in link: #  проверяем эту ссылку
-        id = link.split('/')[-1]  # если да, то получаем его последнюю часть
-    if not id.replace('id', '').isdigit(): # если в нем после отсечения 'id' сами цифры - это и есть id 
-        id = vkapi.utils.resolveScreenName(screen_name=id, v = "5.126")['object_id'] # если нет, получаем id с помощью метода API
+    if 'vk.com/' in link:
+        id = link.split('/')[-1]  
+    if not id.replace('id', '').isdigit(): 
+        id = vkapi.utils.resolveScreenName(screen_name=id, v = "5.126")['object_id']
     else:
         id = id.replace('id', '')
     return int(id)
 
-# count это количество запросов (и количество постов = 100 * count постов)
+
 def getLikes(user_id, cnt, vkapi):
     import time
-    # подписки пользователя
+   
     subscriptions_list = vkapi.users.getSubscriptions(user_id=user_id,extended=0, v = "5.126")['groups']['items']
-    # формируем список id, который нужно передать в следующий метод
+    
     groups_list = ['-' + str(x) for x in subscriptions_list]
     posts = {}
-    # формируем ленту новостей
+  
     newsfeed = vkapi.newsfeed.get(
         filters='post',
         source_ids=', '.join(groups_list),
         count=100, timeout=10, v = '5.126')
    
     posts.update({x['post_id']: x['source_id'] for x in newsfeed['items']})
-    # нужно для получения следующей партии
-    # если требуется более одного запроса — делаем остаток в цикле
+   
     if cnt != 1:
         for c in range(cnt - 1):
             next_from = newsfeed["next_from"]
